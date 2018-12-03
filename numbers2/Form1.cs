241f1108b2542e4f568f5feec1c72e5d853bb1e4
Jack Taylor <RC0026070@student.riversidecollege.ac.uk>
2018-12-03 15:23:59 +0000

    add numbers

--- numbers2/Form1.cs
+++ numbers2/Form1.cs
@@ -15,6 +15,24 @@ namespace numbers2
         public Form1()
         {
             InitializeComponent();
+            double d1 = 5e6;
+            richTextBox1.AppendText(d1.ToString() + "\n");
+            richTextBox1.AppendText(d1.ToString("N") + "\n");
+            richTextBox1.AppendText(d1.ToString("N3") + "\n");
+            richTextBox1.AppendText(d1.ToString("N0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0e0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.00e00") + "\n");
+            double pi = 3.141592653589793;
+            double c = 299792458;
+            richTextBox1.AppendText(pi.ToString("0.000") + "\n");
+            richTextBox1.AppendText(pi.ToString("0.00000") + "\n");
+            richTextBox1.AppendText(pi.ToString("0.00000000000000") + "\n");
+            richTextBox1.AppendText(c.ToString("000000000") + "\n");
+            richTextBox1.AppendText(c.ToString("000000000000000E0") + "\n");
+            richTextBox1.AppendText(c.ToString("0.00e0") + "\n");
         }
+
     }
 }
